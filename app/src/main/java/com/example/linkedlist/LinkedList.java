50f7b10fc45e7910d3af718023d2ec9740acae96
MPB <mp4learning@gmail.com>
2021-10-26 12:37:57 +0530

    Added LinkedList 1. Add node at front 2. Add Node after specific node 3. Add node at the end of list

---
+++ app/src/main/java/com/example/linkedlist/LinkedList.java
@@ -0,0 +1,66 @@
+package com.example.linkedlist;
+
+import android.util.Log;
+
+public class LinkedList {
+
+    Node head = null;
+    Node tail = null;
+
+    public Node createNode(int data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+        return node;
+    }
+
+    public void InsertFrontFront(Node node) {
+//        Node node = createNode(data);
+        node.next = head;
+        head = node;
+    }
+
+    public void insertAfter(Node prev_node, int data) {
+        Node node = createNode(data);
+
+        node.next = prev_node.next;
+        prev_node.next = node;
+    }
+
+    public void insertAtEnd(int data) {
+        Node newNode = createNode(data);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+
+        //Traverse the list till we find the end of list (i.e tail.next = null);
+        newNode.next = null;
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+//      mark next of current last node to point to newlay added node.
+        tail.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            Log.v("LinkedList", "Empty List");
+            return;
+        }
+        while (current != null) {
+            Log.v("LinkedList", "List:" + current.data);
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    class Node {
+        int data;
+        Node next;
+    }
+
+}
---
+++ app/src/main/java/com/example/linkedlist/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.linkedlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinkedList linkedList = new LinkedList();
+
+        LinkedList.Node node = linkedList.createNode(10);
+        linkedList.InsertFrontFront(node);
+
+        LinkedList.Node node1= linkedList.createNode(12);
+        linkedList.InsertFrontFront(node1);
+
+        LinkedList.Node node2 = linkedList.createNode(14);
+        linkedList.InsertFrontFront(node2);
+
+        linkedList.insertAfter(node2,20);
+
+        LinkedList.Node node3= linkedList.createNode(18);
+        linkedList.InsertFrontFront(node3);
+
+        linkedList.insertAtEnd(1023);
+
+        linkedList.display();
+    }
+}
