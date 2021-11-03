e24fed5dcf298412fd5b1113aa6eb66860e16b66
MPB <mp4learning@gmail.com>
2021-11-03 11:24:37 +0530

    Reverse a LinkedList

--- app/src/main/java/com/example/linkedlist/LLPractice.java
+++ app/src/main/java/com/example/linkedlist/LLPractice.java
@@ -93,6 +93,20 @@ public class LLPractice {
 
     }
 
+    void reverseLinkedList() {
+        Node prev=null;
+        Node current = head;
+        Node next;
+
+        while(current!=null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
 
 
 
